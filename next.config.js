7e4220334e01935f5fcc7ce7b32dcb2efd8ac886
MrX8503 <viet_tran@hermanmiller.com>
2023-03-23 09:41:13 -0400

    feature(i18n): add France and Japan

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en-us', 'ja-jp', 'fr-FR'],
+    defaultLocale: 'en-us',
+  }
 }
 
 module.exports = nextConfig
