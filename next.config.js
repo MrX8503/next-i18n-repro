6d430f96b5ac564a6e989aaefb2458ddc42898b4
MrX8503 <viet_tran@hermanmiller.com>
2023-03-24 09:59:30 -0400

    feature(redirects): redirects with next.config and middleware

--- next.config.js
+++ next.config.js
@@ -2,8 +2,23 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['en-us', 'ja-jp', 'fr-FR'],
+    locales: ['en-us', 'ja-jp', 'fr-FR', 'nl-nl', 'es-mx', 'zh-cn'],
     defaultLocale: 'en-us',
+  },
+  redirects() {
+    return [
+      {
+        source: '/nl-NL',
+        destination: '/nl-nl',
+        locale: false,
+        permanent: true
+      },
+      {
+        source: '/es-MX',
+        destination: '/es-mx',
+        permanent: true
+      }
+    ];
   }
 }
 
